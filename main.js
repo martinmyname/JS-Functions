2db8964500f741780439246dd3aa7bdcf2fe0c3f
martin <kaweesimartinanderson@gmail.com>
2019-06-18 08:15:26 -0400

    intro_functions

---
+++ main.js
@@ -0,0 +1,24 @@
+var num=()=>{(Math.floor(Math.random() * 100) + 1)}
+if (num%2==0)
+			alert("The number is even!")
+else (num%2==1)
+	{
+		alert("The number is odd!");
+		}
+for (count = 1;count<=10;count++)
+	{
+	guess= prompt("Guess a number of your choice.");
+	if (guess>num) {
+	alert("Ooops!,its higher")
+}
+else if (guess<num) {
+	alert("Oooh!,its lower")
+}
+
+	if (guess==num)
+	{
+		alert("YOU WIN")
+		break;
+		}
+	if (count==10){
+ alert("YOU LOOSE");}}
